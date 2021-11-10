0f61c38ce8e55b459467c3bd1a7becd8e94aeb10
Ross Barnowski <rossbar@berkeley.edu>
2021-11-09 19:15:13 -0800

    Add requirements and myst-nb extension.

--- source/conf.py
+++ source/conf.py
@@ -28,6 +28,7 @@ author = 'Scientific Python community'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "myst_nb",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
